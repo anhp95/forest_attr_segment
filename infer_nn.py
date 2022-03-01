d113154d5e9757c279c4fe179b5ce1204ff373c8
anhph95@gmail.com <anhph95@gmail.com>
2022-03-01 17:22:35 +0900

    add n_cls to nakat, ena

--- infer_nn.py
+++ infer_nn.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 import argparse
 
-from forest import Forest
+from forest import gen_predicted_map
 from model.deep_forest import DeepForestSpecies
 from tqdm import tqdm
 from utils.checkpoint import load_checkpoint
@@ -17,8 +17,6 @@ BATCH_SIZE = 64
 IMG_SHAPE = (13, 4, 32, 32)
 # IMG_SHAPE = (40, 32, 32)  # 27, 14
 NUM_CLASSES = 4
-FEATURES = [64, 128]
-BACKBONE = "3d_enc_mid_dec_acb"
 INFER_DIR = "data/data_infer"
 
 
@@ -35,6 +33,7 @@ def predict(params):
     acc = params["acc"]
     region = params["region"]
     backbone = params["backbone"]
+    features = params["features"]
 
     checkpoint_file = f"checkpoint/{forest_attr}/{backbone}/0.{acc}.pth.tar"
     in_npy = f"{INFER_DIR}/input/{region}_13b.npy"
@@ -47,8 +46,8 @@ def predict(params):
     model = DeepForestSpecies(
         in_channels=IMG_SHAPE[0],
         out_channels=NUM_CLASSES,
-        backbone=BACKBONE,
-        features=FEATURES,
+        backbone=backbone,
+        features=features,
     ).to(DEVICE)
     load_checkpoint(checkpoint_file, model)
 
@@ -71,17 +70,22 @@ def predict(params):
 
 
 def main():
+
     # parser = argparse.ArgumentParser(
     #     description="Generating High-Resolution Tree Species Map"
     # )
-
     forest_attr = "spec"
-    region = "ena"
-    backbone = BACKBONE
-    acc = 7816
+    backbone = "3d_adj_emd_acb"
+    region = "toki"
+    features = [64, 128]
+    acc = 7780
 
-    low_res_tif = f"data/spec_map/low-res/{region}_{forest_attr}_{backbone}_{acc}.tif"
-    high_res_tif = f"data/spec_map/high-res/{region}_{forest_attr}_{backbone}_{acc}.tif"
+    n_cls = 7  # 10-ena, 7-nakat,
+
+    low_res_tif = (
+        f"data/spec_map/{region}/low-res/{region}_{forest_attr}_{backbone}_{acc}.tif"
+    )
+    high_res_tif = f"data/spec_map/{region}/high-res/{region}_{forest_attr}_{backbone}_{acc}_km{n_cls}.tif"
 
     l2_img_dir = fr"D:\co2_data\DL\large_img\sentinel\s2_{region}_recls\l2"
 
@@ -90,11 +94,15 @@ def main():
         "acc": acc,
         "region": region,
         "backbone": backbone,
+        "features": features,
     }
-    pred_npy_path = predict(params)
+    # pred_npy = predict(params)
 
-    Forest.gen_predicted_map(region, pred_npy_path, low_res_tif, forest_attr)
-    recls(low_res_tif, high_res_tif, l2_img_dir)
+    # gen_predicted_map(region, pred_npy, low_res_tif, forest_attr)
+    recls(low_res_tif, high_res_tif, l2_img_dir, n_cls=n_cls)
 
+
+main()
+# %%
 
 # %%
