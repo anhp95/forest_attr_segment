5845dc499520cbb8379bf7c347e365c3d0968d9d
anhph95@gmail.com <anhph95@gmail.com>
2022-04-21 11:21:07 +0900

    update plot

--- plot/plot.py
+++ plot/plot.py
@@ -10,25 +10,27 @@ csv_dir = "loss_acc_performance"
 figure_dir = "figure"
 
 #%%
-age_file = os.path.join(dir_, csv_dir, "age.csv")
-fig_file = os.path.join(dir_, figure_dir, "age_acc.png")
+attr = "spec"
+csv_file = os.path.join(dir_, csv_dir, f"{attr}.csv")
+fig_file = os.path.join(dir_, figure_dir, f"{attr}_acc.png")
 
-df = pd.read_csv(age_file)
+df = pd.read_csv(csv_file)
 
 epoch = df.epoch.values + 1
 
 acc_2d_p2 = df.acc_2d_p2
 acc_2d_p1p2 = df.acc_2d_p1p2
 acc_2d_p1p2p3 = df.acc_2d_p1p2p3
-acc_3d_p1p2p3 = df.acc_3d_p1p2p3
-acc_new = df.acc_new
+acc_3d_org = df.acc_3d_org_p1p2p3
+acc_3d_adj = df.acc_3d_adj_p1p2p3
 
 fig, ax1 = plt.subplots()
 
-ax1.plot(epoch, acc_2d_p2, "r-", label="UNET (P2)")
-ax1.plot(epoch, acc_2d_p1p2, "g-", label="UNET (P1 + P2)")
-ax1.plot(epoch, acc_2d_p1p2p3, "y-", label="UNET (P1 + P2 + P3)")
-ax1.plot(epoch, acc_3d_p1p2p3, "b-", label="Our model (P1 + P2 + P3)")
+ax1.plot(epoch, acc_2d_p2, "pink", label="2D UNET - P2")
+ax1.plot(epoch, acc_2d_p1p2, "g-", label="2D UNET - P1P2")
+ax1.plot(epoch, acc_2d_p1p2p3, "y-", label="2D UNET - P1P2P3")
+ax1.plot(epoch, acc_3d_org, "r-", label="3D UNET - P1P2P3")
+ax1.plot(epoch, acc_3d_adj, "b-", label="Our model")
 
 ax1.set_xlabel("Number of epochs")
 ax1.set_ylabel("Overall Accuracy")
@@ -36,7 +38,7 @@ ax1.set_ylabel("Overall Accuracy")
 plt.legend()
 plt.show()
 
-fig.savefig(fig_file, format="png", dpi=1200)
+# fig.savefig(fig_file, format="png", dpi=1200)
 
 #%%
 spec_file = os.path.join(dir_, csv_dir, "spec.csv")
@@ -64,4 +66,3 @@ plt.legend()
 plt.show()
 
 fig.savefig(fig_file, format="png", dpi=1200)
-# %%
