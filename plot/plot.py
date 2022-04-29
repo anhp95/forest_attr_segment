4309caac53d676bf963b7af5c973df19e1777a1b
anhph95@gmail.com <anhph95@gmail.com>
2022-04-29 15:27:08 +0900

    update dbf translation

--- plot/plot.py
+++ plot/plot.py
@@ -9,7 +9,6 @@ dir_ = r"D:\Publication\IGARSS 2021\tree_spec\material"
 csv_dir = "loss_acc_performance"
 figure_dir = "figure"
 
-#%%
 attr = "spec"
 csv_file = os.path.join(dir_, csv_dir, f"{attr}.csv")
 fig_file = os.path.join(dir_, figure_dir, f"{attr}_acc.png")
@@ -18,19 +17,25 @@ df = pd.read_csv(csv_file)
 
 epoch = df.epoch.values + 1
 
-acc_2d_p2 = df.acc_2d_p2
-acc_2d_p1p2 = df.acc_2d_p1p2
-acc_2d_p1p2p3 = df.acc_2d_p1p2p3
-acc_3d_org = df.acc_3d_org_p1p2p3
-acc_3d_adj = df.acc_3d_adj_p1p2p3
+fig_2d_p2 = df.acc_2d_p2
+fig_2d_p1p2 = df.acc_2d_p1p2
+fig_2d_p1p2p3 = df.acc_2d_p1p2p3
+fig_3d_org = df.acc_3d_org_p1p2p3
+fig_3d_adj = df.acc_3d_adj_p1p2p3
+
+# fig_2d_p2 = df.loss_2d_p2
+# fig_2d_p1p2 = df.loss_2d_p1p2
+# fig_2d_p1p2p3 = df.loss_2d_p1p2p3
+# fig_3d_org = df.loss_3d_org_p1p2p3
+# fig_3d_adj = df.loss_3d_adj_p1p2p3
 
 fig, ax1 = plt.subplots()
 
-ax1.plot(epoch, acc_2d_p2, "pink", label="2D UNET - P2")
-ax1.plot(epoch, acc_2d_p1p2, "g-", label="2D UNET - P1P2")
-ax1.plot(epoch, acc_2d_p1p2p3, "y-", label="2D UNET - P1P2P3")
-ax1.plot(epoch, acc_3d_org, "r-", label="3D UNET - P1P2P3")
-ax1.plot(epoch, acc_3d_adj, "b-", label="Our model")
+ax1.plot(epoch, fig_2d_p2, "pink", label="2D UNET - P2")
+ax1.plot(epoch, fig_2d_p1p2, "g-", label="2D UNET - P1P2")
+ax1.plot(epoch, fig_2d_p1p2p3, "y-", label="2D UNET - P1P2P3")
+ax1.plot(epoch, fig_3d_org, "r-", label="3D UNET - P1P2P3")
+ax1.plot(epoch, fig_3d_adj, "b-", label="Our model")
 
 ax1.set_xlabel("Number of epochs")
 ax1.set_ylabel("Overall Accuracy")
@@ -39,30 +44,4 @@ plt.legend()
 plt.show()
 
 # fig.savefig(fig_file, format="png", dpi=1200)
-
-#%%
-spec_file = os.path.join(dir_, csv_dir, "spec.csv")
-fig_file = os.path.join(dir_, figure_dir, "spec_acc_new.png")
-df = pd.read_csv(spec_file)
-
-epoch = df.epoch.values + 1
-
-acc_2d_p2 = df.acc_2d_p2
-acc_2d_p1p2 = df.acc_2d_p1p2
-acc_2d_p1p2p3 = df.acc_2d_p1p2p3
-acc_3d_p1p2p3 = df.loss_3d_p1p2p3
-acc_new = df.loss_new
-
-fig, ax1 = plt.subplots()
-
-
-ax1.plot(epoch, acc_3d_p1p2p3, "b-", label="Old model")
-ax1.plot(epoch, acc_new, "g-", label="Improved model")
-
-ax1.set_xlabel("Number of epochs")
-ax1.set_ylabel("Loss")
-
-plt.legend()
-plt.show()
-
-fig.savefig(fig_file, format="png", dpi=1200)
+# %%
--- preprocessing/translate_dbf.py
+++ preprocessing/translate_dbf.py
@@ -45,42 +45,119 @@ def handleInvalidText(text):
         return text
 
 
-def spec_to_label(spec):
-    if spec == "Sugi":
-        return 1
-    elif spec == "Cypress":
-        return 2
-    elif spec == "Pine":
-        return 3
-    elif spec == "C":
-        return 4
-    elif spec == "BF":
-        return 5
-    return 6
+class MainForestAttr:
+    def __init__(self, s1, s2, s3, ag1, ag2, ag3, r1, r2, r3):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        self.ag1 = ag1
+        self.ag2 = ag2
+        self.ag3 = ag3
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
+
+        self.main_spec = []
+        self.main_age = []
+
+        self.trans_spec = []
+        self.cls_age = []
+        self.cls_spec = []
+
+        self._get_main_attr()
+        self._do_cls_age()
+
+        self._do_trans_spec()
+        self._do_cls_spec()
+
+    def _get_main_attr(self):
+        agg_ratio = [[r1, r2, r3] for r1, r2, r3 in zip(self.r1, self.r2, self.r3)]
+        agg_spec = [[s1, s2, s3] for s1, s2, s3 in zip(self.s1, self.s2, self.s3)]
+        agg_age = [
+            [ag1, ag2, ag3] for ag1, ag2, ag3 in zip(self.ag1, self.ag2, self.ag3)
+        ]
+        for spec, age, ratio in zip(
+            agg_spec,
+            agg_age,
+            agg_ratio,
+        ):
+            main_ratio = max(ratio)
+            main_ratio_index = ratio.index(main_ratio)
+            self.main_age.append(age[main_ratio_index])
+            self.main_spec.append(spec[main_ratio_index])
+
+    def _do_cls_age(self):
+        for age in self.main_age:
+            if age <= 20:
+                cls_age = 1
+            elif age > 21 and age < 50:
+                cls_age = 2
+            elif age >= 50:
+                cls_age = 3
+            self.cls_age.append(cls_age)
+
+    def _do_trans_spec(self):
+        unique_spec = list(set(self.main_spec))
+
+        trans_unique_spec = [handleInvalidText(spec) for spec in unique_spec]
+        trans_dict = {key: val for key, val in zip(unique_spec, trans_unique_spec)}
+        self.trans_spec = [
+            trans_dict[spec]
+            if trans_dict[spec] not in TREE_SPEC
+            else TREE_SPEC[trans_dict[spec]]
+            for spec in self.main_spec
+        ]
+
+    def _do_cls_spec(self):
+        for spec in self.trans_spec:
+            cls_spec = 0
+            if spec == "Sugi":
+                cls_spec = 1
+            elif spec == "BF":
+                cls_spec = 2
+            elif spec == "Pine" or spec == "C":
+                cls_spec = 3
+            elif spec == "Cypress":
+                cls_spec = 4
+
+            self.cls_spec.append(cls_spec)
+
+    def gen_from_df(df):
+        return MainForestAttr(
+            df.SPEC1.tolist(),
+            df.SPEC2.tolist(),
+            df.SPEC3.tolist(),
+            df.AGE1.tolist(),
+            df.AGE2.tolist(),
+            df.AGE3.tolist(),
+            df.RATIO1.tolist(),
+            df.RATIO2.tolist(),
+            df.RATIO3.tolist(),
+        )
+
+    def to_df(self):
+        df = pd.DataFrame()
+
+        df["main_spec"] = self.main_spec
+        df["main_age"] = self.main_age
+
+        df["trans_spec"] = self.trans_spec
+        df["cls_age"] = self.cls_age
+        df["cls_spec"] = self.cls_spec
+
+        return df
 
 
-def translate_dbf(dbf_file):
+def main(dbf_file):
     df = Dbf5(dbf_file, codec="shift-jis").to_dataframe()
-    org_spec = df.SPEC1.tolist()
-    unique_spec = list(set(org_spec))
-
-    trans_unique_spec = [handleInvalidText(spec) for spec in unique_spec]
-    trans_dict = {key: val for key, val in zip(unique_spec, trans_unique_spec)}
-    refine_spec = [
-        trans_dict[spec]
-        if trans_dict[spec] not in TREE_SPEC
-        else TREE_SPEC[trans_dict[spec]]
-        for spec in org_spec
-    ]
-
-    label_spec = [spec_to_label(spec) for spec in refine_spec]
-    new_df = pd.DataFrame()
-    new_df["spec"] = refine_spec
-    new_df["label"] = label_spec
-    new_df.to_csv(dbf_file.replace(".dbf", ".csv"))
+    main_forest_attr = MainForestAttr.gen_from_df(df)
+    df_MFA = main_forest_attr.to_df()
+    return df_MFA
 
 
-#%%
-# if __name__ == '__main__':
-# dbf_file = r"D:\Takejima-sensei\ena_private_forest\2018_all\backup\2018_all.dbf"
-# translate_dbf(dbf_file)
+dbf_file = r"D:\Takejima-sensei\ena_private_forest\all_en\2018_all.dbf"
+df = main(dbf_file)
+print(df)
+
+
+# %%
